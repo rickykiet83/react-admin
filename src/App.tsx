3e307bcb2cbbe17c97d4560976843c3a18ae2b35
Kiet Pham <kietpham.dev@gmail.com>
2021-09-15 14:43:09 +1000

    create Login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,20 @@
 import './App.css';
 
-import Menu from './components/Menu';
-import Nav from './components/Nav';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 import React from 'react';
+import Register from './pages/Register';
+import Users from './pages/Users';
 
 function App() {
   return (
     <div className='App'>
-      <Nav />
-      <div className='container-fluid'>
-        <div className='row'>
-          <Menu />
-          <main className='col-md-9 ms-sm-auto col-lg-10 px-md-4'>
-            <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
-              <h1 className='h2'>Dashboard</h1>
-            </div>
-            <h2>Section title</h2>
-            <div className='table-responsive'>
-              <table className='table table-striped table-sm'>
-                <thead>
-                  <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Header</th>
-                    <th scope='col'>Header</th>
-                    <th scope='col'>Header</th>
-                    <th scope='col'>Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route path={'/'} exact component={Users}></Route>
+        <Route path={'/login'} component={Login}></Route>
+        <Route path={'/register'} component={Register}></Route>
+      </BrowserRouter>
     </div>
   );
 }
