f610efec985ff4720a985877e3285a18a6f72d42
Kiet Pham <kietpham.dev@gmail.com>
2021-09-15 14:29:33 +1000

    create nav, menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Menu from './components/Menu';
+import Nav from './components/Nav';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav />
+      <div className='container-fluid'>
+        <div className='row'>
+          <Menu />
+          <main className='col-md-9 ms-sm-auto col-lg-10 px-md-4'>
+            <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
+              <h1 className='h2'>Dashboard</h1>
+            </div>
+            <h2>Section title</h2>
+            <div className='table-responsive'>
+              <table className='table table-striped table-sm'>
+                <thead>
+                  <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Header</th>
+                    <th scope='col'>Header</th>
+                    <th scope='col'>Header</th>
+                    <th scope='col'>Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
